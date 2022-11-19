480ccc43a0e40a1413952f3640cf7beb24ff5c20
ThMeloDev <thiagomelo0509@gmail.com>
2022-11-18 22:13:12 -0300

    I created Login Screen and Home Screen

---
+++ src/butchersystem/ButcherSystem.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package butchersystem;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ButcherSystem {
+
+    Login login = new Login();
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
